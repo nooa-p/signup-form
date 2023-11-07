a701b2577d57386285e97b9bd129318b4dd1852e
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-07 13:34:11 +0200

    form textfields styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
           </div>
           <div className='bg-white p-6 lg:p-10 rounded-lg shadow'>
             <form className=''>
-              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-gray focus:text-dark' name="" id="" placeholder='First Name' />
-              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-gray focus:text-dark' name="" id="" placeholder='Last Name' />
-              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-gray focus:text-dark' name="" id="" placeholder='Email Address' />
-              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-gray focus:text-dark' name="" id="" placeholder='Password'  />
+              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark' name="" id="" placeholder='First Name' />
+              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark' name="" id="" placeholder='Last Name' />
+              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark' name="" id="" placeholder='Email Address' />
+              <input type="text" className='block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark' name="" id="" placeholder='Password'  />
               <button className='bg-green text-white shadow-sm block w-full p-3 rounded uppercase font-medium tracking-wider hover:bg-green-light border border-green-dark'>Claim your free trial</button>
             </form>
             <span className='block w-full text-center text-[11px] mt-4 text-gray font-medium'>By clicking the button, you are agreeing to our <a href="#" className='font-semibold text-red'>Terms and Services</a></span>
