0209961201a883ecdd4b44d8d2bd424a3fbb2c16
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-09 10:32:56 +0200

    changed layout change to smaller screen, styling and positioning error icon and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ const Home = () => {
   };
 
   const finishSubmit = () => {
-    alert('Submitted successfully!');
+    console.log('submitted successfully');
   }
 
   useEffect(() => {
@@ -55,17 +55,17 @@ const Home = () => {
   })
 
   return (
-    <main className="flex flex-col lg:flex-row max-w-6xl mt-10 lg:mt-0 lg:h-screen lg:items-center mx-auto gap-10 lg:gap-6 p-8 pb-0">
-      <div className="flex flex-col content-evenly gap-6 lg:flex-1 lg:flex-grow">
-        <h1 className="text-center lg:text-left text-white text-3xl lg:text-5xl font-bold">
+    <main className="flex flex-col md:flex-row max-w-6xl mt-10 md:mt-0 md:h-screen md:items-center mx-auto gap-10 md:gap-6 p-8 pb-0">
+      <div className="flex flex-col content-evenly gap-6 md:flex-1 md:flex-grow">
+        <h1 className="text-center md:text-left text-white text-3xl md:text-5xl font-bold">
           Learn to code by watching others
         </h1>
-        <p className="text-center lg:text-left text-white font-medium">
+        <p className="text-center md:text-left text-white font-medium">
           See how experienced developers solve problems in real-time. Watching
           scripted tutorials is great, but understanding how developers think is
           invaluable.
         </p>
-        <p className="text-center lg:text-left text-white text-[11px]">
+        <p className="text-center md:text-left text-white text-[11px]">
           Challenge by{" "}
           <a
             href="https://www.frontendmentor.io?ref=challenge"
@@ -81,45 +81,81 @@ const Home = () => {
           .
         </p>
       </div>
-      <div className="flex flex-col gap-4 lg:flex-1 lg:flex-grow">
-        <div className="bg-blue text-white py-4 px-20 lg:px-0 text-center rounded-lg shadow">
+      <div className="flex flex-col gap-4 md:flex-1 md:flex-grow">
+        <div className="bg-blue text-white py-4 px-20 md:px-0 text-center rounded-lg shadow">
           <span className="font-semibold">Try it free 7 days</span> then $20/mo.
           thereafter
         </div>
-        <div className="bg-white p-6 lg:p-10 rounded-lg shadow">
-          <form className="" onSubmit={handleSubmit}>
+        <div className="bg-white p-6 md:p-10 rounded-lg shadow">
+          <form className="relative" onSubmit={handleSubmit}>
+            <span 
+              className='inline-block bg-[url("/icon-error.svg")] w-6 h-6 absolute right-6 top-3.5'
+            >
+            </span>
             <input
               type="text"
-              className={`block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.first ? 'border-2 border-red' : ''}`}
+              className={`block w-full p-3.5 pl-7 rounded ${errors.first ? 'mb-5' : ''} border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.first ? 'border-2 border-red' : ''}`}
               placeholder="First Name"
               name='first'
               onChange={handleChange}
               value={inputFields.first}
             />
+            <span
+              className='inline-block w-full text-right mt-1 mb-3 text-[11px] text-red italic font-semibold'
+            >
+              test
+            </span>
+            <span 
+              className='inline-block bg-[url("/icon-error.svg")] w-6 h-6 absolute right-6 top-[103px]'
+            >
+            </span>
             <input
               type="text"
-              className={`block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.last ? 'border-2 border-red' : ''}`}
+              className={`block w-full p-3.5 pl-7 rounded ${errors.last ? 'mb-5' : ''} border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.last ? 'border-2 border-red' : ''}`}
               placeholder="Last Name"
               name='last'
               onChange={handleChange}
               value={inputFields.last}
             />
+            <span
+              className='inline-block w-full text-right mt-1 mb-3 text-[11px] text-red italic font-semibold'
+            >
+              test
+            </span>
+            <span 
+              className='inline-block bg-[url("/icon-error.svg")] w-6 h-6 absolute right-6 top-[150px]'
+            >
+            </span>
             <input
               type="text"
-              className={`block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.email ? 'border-2 border-red' : ''}`}
+              className={`block w-full p-3.5 pl-7 rounded ${errors.email ? 'mb-5' : ''} border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.email ? 'border-2 border-red' : ''}`}
               placeholder="Email Address"
               name='email'
               onChange={handleChange}
               value={inputFields.email}
             />
+            <span
+              className='inline-block w-full text-right mt-1 mb-3 text-[11px] text-red italic font-semibold'
+            >
+              test
+            </span>
+            <span 
+              className='inline-block bg-[url("/icon-error.svg")] w-6 h-6 absolute right-6'
+            >  
+            </span>
             <input
               type="password"
-              className={`block w-full p-3.5 pl-7 rounded mb-5 border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.password ? 'border-2 border-red' : ''}`}
+              className={`block w-full p-3.5 pl-7 rounded ${errors.password ? 'mb-5' : ''} border border-gray-light placeholder:text-dark-light placeholder:font-semibold focus:font-semibold focus:border-dark focus:text-dark focus:outline-none font-semibold text-dark ${errors.password ? 'border-2 border-red' : ''}`}
               placeholder="Password"
               name='password'
               onChange={handleChange}
               value={inputFields.password}
             />
+            <span
+              className='inline-block w-full text-right mt-1 mb-3 text-[11px] text-red italic font-semibold'
+            >
+              test
+            </span>
             <button className="bg-green text-white shadow-sm block w-full p-3 rounded uppercase font-medium tracking-wider hover:bg-green-light border border-green-dark" onClick={handleSubmit}>
               Claim your free trial
             </button>
